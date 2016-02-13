7913d396fd158dc2d7c58eeb69997fde2d9969e7
wesleymccloy <wesleymccloy@gmail.com>
2016-02-13 13:25:08 -0500

    Sample class file

---
+++ wayfarer/Sample.cpp
@@ -0,0 +1,11 @@
+/*
+	Sample.cpp sample
+*/
+
+#include "Arduino.h"
+#include "Sample.h"
+
+Sample::Sample() : rightInnerWheelRadius(defaultRightInnerWheelRadius)
+{
+	
+}
---
+++ wayfarer/Sample.h
@@ -0,0 +1,19 @@
+/*
+	Sample.h header file
+*/
+
+#ifndef Sample_h
+#define Sample_h
+
+#include "Arduino.h"
+
+class Sample
+{
+public:
+	Sample();
+	double variable;
+private:
+
+};
+
+#endif
