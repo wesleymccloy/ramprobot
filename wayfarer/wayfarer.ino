6555a99833dd3563acae12fc1497cd7a11dbaaca
nancibles <nancy.pham.95@gmail.com>
2016-03-01 23:16:07 -0500

    ir

--- wayfarer/wayfarer.ino
+++ wayfarer/wayfarer.ino
@@ -4,12 +4,32 @@
 ServoWayfarer leftServo(44,96,0);
 ServoWayfarer rightServo(46,98,1);
 
+#define irSense A0; 
+int distance = 0;
+double distanceToRead = 0.0; 
+
 void setup() {
+  Serial.begin(9600); 
 }
 
 int pos = 0;
+
 void loop() {
-leftServo.percentSpeed(100);
-rightServo.percentSpeed(100);
-delay(1000);
+  
+  distance = analogRead(irSense); 
+  distanceToRead = (2061.19) / pow(distance, 0.983284);
+  if ( (distanceToRead < 4) || (distanceToRead > 30) )
+  {  
+    leftServo.percentSpeed(100);
+    rightServo.percentSpeed(100);
+  }
+  
+  else
+  {
+    leftServo.percentSpeed(0); 
+    rightServo.percentSpeed(0); 
+  }
+  
+  delay(200);
+  
 }
